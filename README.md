ecf3cb565a247b8e1e3454c3dec295d9c459b885
Phelps <phelpsmemo@gmail.com>
2017-09-30 09:06:30 -0300

    Adição de menu, e términos das questões

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Quiz com perguntas sobre economia e finanças
 
+## Objetivo
+
+Criado para auxiliar a dinamizar o estudo das questões propostas nas aulas de economia e finanças.
+
 ## Informações
 
-* Quantidade de questões: 50
-* Questões respondidas: 50
+* Quantidade de questões: 100
+* Questões respondidas: 100
--- quiz-economia.py
+++ quiz-economia.py
@@ -12,15 +12,27 @@ elif sys == 'Windows':
 with open('economia.json') as dados:
     data = json.load(dados)
 
+
+def question(inicio, fim):
+    for j in range(inicio, fim):
+        print("Veja se conseque responder...")
+        print("Pergunta: " + str(data['economics']['quests'][j]['pergunta']))
+        input("Aperte a tecla Enter para ver a resposta")
+        print("Resposta: " + str(data['economics']['quests'][j]['resposta']))
+        input("Aperte a tecla Enter para a próxima pergunta")
+        subprocess.run([command])
+
 subprocess.run([command])
 
-for i in data['economics']['quests']:
+choose = int(input("Escolha:\n1 - Responder primeiras 50 questões\n2 - Responder as ultimas 50 questões\n3 - Todas as questões\n"))
 
-    print("Veja se conseque responder...")
-    print("Pergunta: " + str(i['pergunta']))
-    input("Aperte qualquer tecla para ver a resposta")
-    print("Resposta: " + str(i['resposta']))
-    input("Aperte qualquer tecla para a próxima pergunta")
-    subprocess.run([command])
+if choose == 1:
+    question(0, 50)
 
+elif choose == 2:
+    question(51, 100)
+
+elif choose == 3:
+    question(0, 100)
+        
 print("Fim =D")

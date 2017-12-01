cc15d18c8144ac36d364112801d07aff2a926095
Phelps <phelpsmemo@gmail.com>
2017-12-01 17:22:50 -0200

    Remoção de método dispensável

--- util/question.py
+++ util/question.py
@@ -7,14 +7,7 @@ class Question():
         with open(path) as dados:
             self.data = json.load(dados)
 
-    def __verify_data(self):
-        if self.data == '':
-            print('Erro ao ler o arquivo')
-            exit()
-
     def question(self, inicio, fim):
-        self.__verify_data()
-
         for j in range(inicio, fim):
             print("Veja se conseque responder...")
             print("Pergunta: " + str(self.data['economics']['quests'][j]['pergunta']))
